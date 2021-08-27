91ab1188e6c35275f51282baac1b46e53a2b68ac
Sagnik Roy <58679327+sagnik1511@users.noreply.github.com>
2021-08-27 13:06:24 +0530

    Update blocks.py

--- blocks.py
+++ blocks.py
@@ -1,70 +1,87 @@
 import torch
 import torch.nn as nn
 
-def conv(cin, out, kernel_size = (3, 3), padding = (1, 1), stride = 1):
-    return nn.Conv2d(in_channels = cin, out_channels = out, kernel_size = kernel_size, padding = padding, stride = stride)
+
+"""
+Primary model blocks to create the base blocks
 
 
-def first_block(data, kernel_size = (3,3), padding = (1, 1), stride = 1, pool = "avg"):
+"""
 
-    mode1 = nn.Sequential(
-            conv(cin = data.shape[1], out = 32, padding = padding, stride = stride),
-            nn.BatchNorm2d(num_features = 32, momentum = 0.9),
+def Conv(in_channels, out_channels, kernel_size, stride, padding):
+    return nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+
+
+class PCBA(nn.Module):
+    def __init__(self,
+                 in_channels = 150,
+                 out_channels = 32,
+                 kernel_size = (3, 3),
+                 stride = 1,
+                 padding = 1,
+                 momentum = 0.9):
+        super().__init__()
+        self.pcba = nn.Sequential(
+            nn.MaxPool2d(kernel_size = (2, 2)),
+            Conv(in_channels, out_channels, kernel_size, stride, padding),
             nn.ReLU(),
-        )
-    if pool == 'avg':
-        mode2 = nn.Sequential(
-            nn.AvgPool2d(kernel_size = kernel_size),
-            conv(cin = data.shape[1], out = 32, padding = padding, stride = stride),
-            nn.BatchNorm2d(num_features = 32, momentum=0.9),
-            nn.ReLU(),
-        )
-    else:
-        mode2 = nn.Sequential(
-            nn.MaxPool2d(kernel_size=kernel_size),
-            conv(cin = data.shape[1], out = 32, padding = padding, stride = stride),
-            nn.BatchNorm2d(num_features = 32, momentum=0.9),
-            nn.ReLU(),
-        )
-    return (mode1(data), mode2(data))        # normal , lowered
-
-
-def second_block(data, kernel_size = (3, 3), padding = (1, 1), stride = 1, pool = "avg", downsample = True):
-
-    if downsample == True:
-        return first_block(data = data, kernel_size = kernel_size, padding = padding, stride = stride, pool = pool)
-
-    else:
-        mode1 = nn.Sequential(
-            conv(cin=data.shape[1], out=32, padding=padding, stride=stride),
-            nn.BatchNorm2d(num_features=32, momentum=0.9),
-            nn.ReLU(),
-        )
-        mode2 = nn.Sequential(
-            nn.Upsample(scale_factor = kernel_size[0]),
-            conv(cin=data.shape[1], out=32, padding=padding, stride=stride),
-            nn.BatchNorm2d(num_features=32, momentum=0.9),
-            nn.ReLU(),
+            nn.BatchNorm2d(num_features = out_channels, momentum = momentum),
         )
 
-    return (mode1(data), mode2(data))   # normal , lowered / upsampled
+    def forward(self, x):
+        return self.pcba(x)
 
-def third_block(data, kernel_size = (3, 3), padding = (1, 1), stride = 1, upsample = True):
 
-    if upsample == True:
-        return nn.Sequential(
-            nn.Upsample(scale_factor = kernel_size[0]),
-            conv(cin=data.shape[1], out=32, padding=padding, stride=stride),
-            nn.BatchNorm2d(num_features=32, momentum=0.9),
+class CBA(nn.Module):
+    def __init__(self,
+                 in_channels=150,
+                 out_channels=32,
+                 kernel_size=(3, 3),
+                 stride=1,
+                 padding=1,
+                 momentum=0.9):
+        super().__init__()
+        self.cba = nn.Sequential(
+            Conv(in_channels, out_channels, kernel_size, stride, padding),
             nn.ReLU(),
-        )(data)  # lowered
-    else:
-        return nn.Sequential(
-            conv(cin=data.shape[1], out=32, padding=padding, stride=stride),
-            nn.BatchNorm2d(num_features=32, momentum=0.9),
+            nn.BatchNorm2d(num_features=out_channels, momentum=momentum),
+        )
+
+    def forward(self, x):
+        return self.cba(x)
+
+
+class UCBA(nn.Module):
+    def __init__(self,
+                 in_channels=150,
+                 out_channels=32,
+                 kernel_size=(3, 3),
+                 stride=1,
+                 padding=1,
+                 momentum=0.9):
+        super().__init__()
+        self.ucba = nn.Sequential(
+            nn.Upsample(scale_factor = 2),
+            Conv(in_channels, out_channels, kernel_size, stride, padding),
             nn.ReLU(),
-        )(data)   # upsampled
+            nn.BatchNorm2d(num_features=out_channels, momentum=momentum),
+        )
+
+    def forward(self, x):
+        return self.ucba(x)
 
 
+class BAD(nn.Module):
+    def __init__(self,
+                 in_channels,
+                 momentum = 0.9,
+                 dropout = 0.1):
+        super().__init__()
+        self.bad = nn.Sequential(
+            nn.BatchNorm2d(num_features = in_channels, momentum = momentum),
+            nn.ReLU(),
+            nn.Dropout(p = dropout),
+        )
 
-
+    def forward(self, x):
+        return self.bad(x)
